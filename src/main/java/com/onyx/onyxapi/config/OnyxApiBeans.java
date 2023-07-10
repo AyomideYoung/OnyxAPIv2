2b3242c71740df9912a89da8713933bfd3505543
habit <ayomidebakare301@gmail.com>
2023-07-10 05:57:25 +0100

    Created 2 Spring provided task executors

--- src/main/java/com/onyx/onyxapi/config/OnyxApiBeans.java
+++ src/main/java/com/onyx/onyxapi/config/OnyxApiBeans.java
@@ -2,13 +2,33 @@ package com.onyx.onyxapi.config;
 
 import com.onyx.onyxapi.exception.OnyxApiControllerAdvice;
 import com.onyx.onyxapi.service.config.OnyxServiceBeans;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@EnableAsync
 @Import({
         RestAPIControllers.class,
         OnyxApiControllerAdvice.class,
         OnyxServiceBeans.class,
 })
 @Configuration
-public class OnyxApiBeans {}
+public class OnyxApiBeans {
+
+    //previously callbackExecutorService
+    @Bean
+    public TaskExecutor executorService(){
+       var executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(8);
+        executor.setMaxPoolSize(24);
+        executor.setQueueCapacity(128);
+        return executor;
+    }
+}
--- src/main/java/com/onyx/onyxapi/dao/config/OnyxDAOBeans.java
+++ src/main/java/com/onyx/onyxapi/dao/config/OnyxDAOBeans.java
@@ -1,6 +1,21 @@
 package com.onyx.onyxapi.dao.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
-public class OnyxDAOBeans {}
+public class OnyxDAOBeans {
+
+
+    @Bean
+    public TaskExecutor persistenceTaskExecutor(){
+        var exec = new ThreadPoolTaskExecutor();
+        exec.setCorePoolSize(8);
+        exec.setMaxPoolSize(24);
+        exec.setQueueCapacity(128);
+        return exec;
+    }
+
+}
